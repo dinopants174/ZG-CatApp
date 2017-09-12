ff10bc45641c850135b65207df3e946dda87c8bf
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2017-09-12 11:34:16 -0700

    Source maps now working, gross solution

--- copyStaticAssets.js
+++ copyStaticAssets.js
@@ -1,5 +1,7 @@
 var shell = require('shelljs');
 
+shell.mkdir('-p', 'dist/public/src/public');
 shell.cp('-R', 'src/public/images', 'dist/public/');
 shell.cp('-R', 'src/public/styles', 'dist/public/');
 shell.cp('-R', 'src/public/javascripts/lib', 'dist/public/javascripts/');
+shell.cp('-R', 'src/public/javascripts', 'dist/public/src/public/');
