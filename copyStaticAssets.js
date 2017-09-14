5f7b07294f865fa28fd4c134e312f93d238254af
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2017-09-14 11:56:32 -0700

    Cleaned up gross solution but still gross

--- copyStaticAssets.js
+++ copyStaticAssets.js
@@ -5,3 +5,4 @@ shell.cp('-R', 'src/public/images', 'dist/public/');
 shell.cp('-R', 'src/public/styles', 'dist/public/');
 shell.cp('-R', 'src/public/javascripts/lib', 'dist/public/javascripts/');
 shell.cp('-R', 'src/public/javascripts', 'dist/public/src/public/');
+shell.rm('-rf', 'dist/public/src/public/javascripts/lib');
