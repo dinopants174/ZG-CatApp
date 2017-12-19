552cbbe2f0c1e35a7cf07933036eb4b6cb923f6f
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2017-12-19 10:26:19 -0800

    For F12 sourcemap issue:

--- copyStaticAssets.js
+++ copyStaticAssets.js
@@ -1,8 +1,12 @@
 var shell = require('shelljs');
 
-shell.mkdir('-p', 'dist/public/src/public');
+// uncomment to make source map work in Edge DevTools
+// shell.mkdir('-p', 'dist/public/src/public');
+
 shell.cp('-R', 'src/public/images', 'dist/public/');
 shell.cp('-R', 'src/public/styles', 'dist/public/');
 shell.cp('-R', 'src/public/javascripts/lib', 'dist/public/javascripts/');
-shell.cp('-R', 'src/public/javascripts', 'dist/public/src/public/');
-shell.rm('-rf', 'dist/public/src/public/javascripts/lib');
+
+// uncomment to make source map work in Edge DevTools
+// shell.cp('-R', 'src/public/javascripts', 'dist/public/src/public/');
+// shell.rm('-rf', 'dist/public/src/public/javascripts/lib');
